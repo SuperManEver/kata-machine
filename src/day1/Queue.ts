154924a950a5ccb0448655ad71419664924d56be
n.luparev <nikita.luparev@gmail.com>
2023-10-30 23:24:01 +0500

    some progress on queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,54 @@
+class Node<T> {
+  public value: T
+  public next?: Node<T>
+
+  constructor(value: T) {
+    this.value = value
+  }
+}
+
 export default class Queue<T> {
-    public length: number;
+  public length: number
+  private head?: Node<T>
+  private tail?: Node<T>
 
-    
+  constructor() {
+    this.head = this.tail = undefined
+    this.length = 0
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    this.length++
+
+    const newNode = new Node(item)
+
+    if (!this.tail) {
+      this.tail = this.head = newNode
+      return
     }
 
-    enqueue(item: T): void {
+    this.tail.next = newNode
+    this.tail = newNode
+  }
 
-}
-    deque(): T | undefined {
+  deque(): T | undefined {
+    if (!this.head) {
+      return void 0
+    }
 
-}
-    peek(): T | undefined {
+    this.length--
 
-}
+    const temp = this.head
+    this.head = this.head.next
+
+    if (this.length === 0) {
+      this.tail === void 0
+    }
+
+    return temp.value
+  }
+
+  peek(): T | undefined {
+    return this.head?.value
+  }
 }
