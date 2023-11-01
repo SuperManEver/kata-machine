257bd7f578aac97eb1aed1297132795848dc1c7a
n.luparev <nikita.luparev@gmail.com>
2023-11-01 19:14:07 +0500

    implemented stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,46 @@
+type Node<T> = {
+  value: T
+  prev?: Node<T>
+}
+
 export default class Stack<T> {
-    public length: number;
+  public length: number
+  private head?: Node<T>
 
-    
+  constructor() {
+    this.head = void 0
+    this.length = 0
+  }
 
-    constructor() {
+  push(item: T): void {
+    const newNode: Node<T> = {
+      value: item,
     }
 
-    push(item: T): void {
+    this.length++
 
-}
-    pop(): T | undefined {
+    if (!this.head) {
+      this.head = newNode
+      return
+    }
 
-}
-    peek(): T | undefined {
+    newNode.prev = this.head
+    this.head = newNode
+  }
 
-}
+  pop(): T | undefined {
+    if (!this.head) {
+      return void 0
+    }
+
+    const temp = this.head
+    this.head = this.head.prev
+    this.length--
+
+    return temp.value
+  }
+
+  peek(): T | undefined {
+    return this.head?.value
+  }
 }
