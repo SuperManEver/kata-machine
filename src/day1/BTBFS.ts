15dc55d943e2924b08b2993745a27322238ffe3d
Nik Luparev <nikita.luparev@gmail.com>
2023-11-17 00:43:14 +0500

    breadth first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,22 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const queue = [head]
 
+  while (queue.length) {
+    const curr = queue.shift() as BinaryNode<number>
+
+    // search
+    if (curr.value === needle) {
+      return true
+    }
+
+    if (curr.left) {
+      queue.push(curr.left)
+    }
+
+    if (curr.right) {
+      queue.push(curr.right)
+    }
+  }
+
+  return false
 }
